3264615b96aa640c68a2dc87f87fe85d13086077
Roni Palacio <roninfo@gmail.com>
2017-04-10 22:55:36 -0300

    adiciona valor recebido por query string em uma lista

--- tarefas.php
+++ tarefas.php
@@ -5,6 +5,24 @@
 </head>
 <body>
 	<h1>Gerenciador de tarefas</h1>
+	<form>
+		<fieldset>
+			<legend>
+				Nova tarefa
+			</legend>
+			<label>Tarefa: <input type="text" name="nome"></label>
+			<input type="submit" name="Cadastrar">
+		</fieldset>
+	</form>
+	<?php 
+
+		$lista_tarefas = array();
+
+		if(isset($_GET['nome'])) {
+			$lista_tarefas[] = $_GET['nome'];	
+		}
+		
+	?>
 
 </body>
 </html>
