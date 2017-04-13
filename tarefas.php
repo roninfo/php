5d7281813e492c6d8daeca5a0986034f027ff331
roni.ferreira <roni.ferreira@reservafacil.tur.br>
2017-04-13 16:15:33 -0300

    Start na session e adiciona session para guardar as tarefas

--- tarefas.php
+++ tarefas.php
@@ -16,13 +16,28 @@
 	</form>
 	<?php 
 
+		session_start();
 		$lista_tarefas = array();
 
 		if(isset($_GET['nome'])) {
-			$lista_tarefas[] = $_GET['nome'];	
+			$_SESSION['lista_tarefas'][] = $_GET['nome'];	
 		}
 		
+		if(isset($_SESSION['lista_tarefas'])) {
+			$lista_tarefas = $_SESSION['lista_tarefas'];
+		}
 	?>
+
+	<table>
+		<tr>
+			<th>Tarefas</th>
+		</tr>
+		<?php foreach ($lista_tarefas as $tarefa) : ?>
+			<tr>
+				<td><?php echo $tarefa ?></td>
+			</tr>
+		<?php endforeach; ?>
+	</table>
 
 </body>
 </html>
